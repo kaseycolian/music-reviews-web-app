e51943c202984ad2089f175709247e38d34097aa
Kasey <KaseyColian@gmail.com>
2018-03-02 09:47:38 -0500

    initial commit

---
+++ src/main/java/org/wecancodeit/columbus/ReviewsFull/ReviewsFullApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.columbus.ReviewsFull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewsFullApplication.class, args);
+	}
+}
---
+++ src/test/java/org/wecancodeit/columbus/ReviewsFull/ReviewsFullApplicationTests.java
@@ -0,0 +1,16 @@
+package org.wecancodeit.columbus.ReviewsFull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReviewsFullApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
